79cfa3ae4b8bc0a9b287086b31bd9d0d46291cd3
Lewis Matos <lewiismb1@gmail.com>
2020-04-21 10:47:38 -0400

    todo bien

--- app/src/main/java/com/example/pxstie/BuscarFragment.java
+++ app/src/main/java/com/example/pxstie/BuscarFragment.java
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+public class BuscarFragment extends Fragment implements View.OnClickListener{
 
-public class OpcionesFragment extends Fragment implements View.OnClickListener{
-
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=  inflater.inflate(R.layout.fragment_opciones, container, false);
+        View view =  inflater.inflate(R.layout.fragment_buscar, container, false);
 
         return view;
     }
