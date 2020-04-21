9f59acd0a5832ea3a70c86060b7eaaca5157dabc
Lewis Matos <lewiismb1@gmail.com>
2020-04-21 08:14:48 -0400

    todo bien

--- app/src/main/java/com/example/pxstie/MasInfoActivity.java
+++ app/src/main/java/com/example/pxstie/MasInfoActivity.java
@@ -24,7 +24,7 @@ public class MasInfoActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnVolver:
-                startActivity(new Intent(this, CuentaActivity.class));
+                startActivity(new Intent(this, CuentaFragment.class));
                 finish();
                 break;
         }
